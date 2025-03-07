e9eb23a3e5588912f3c7fd28999d9d6db6f93bcb
unknown <akash101811@gmail.com>
2025-03-07 15:42:20 +0530

    db fixes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         // add this to env
-        await mongoose.connect("mongodb+srv://akash101811:o9FkYY8ipjB5eS26@cluster0.vgds6.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("MongoDB connected");
     } catch (error) {
         console.log("MongoDB Connection Error", error);
